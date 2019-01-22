0eddb04af4a381634680ccebb6bade85b0b93730
Brett Peterson <pstlpete58@yahoo.com>
2019-01-22 10:50:40 -0800

    Add project files.

---
+++ eCommereceSite/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace eCommereceSite.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
---
+++ eCommereceSite/Models/Member.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace eCommerceSite.Models
+{
+    public class Member
+    {
+        //Data Annotations validation and DB markep
+        [Required]
+        public string Name { get; set; }
+
+        [Required]
+        [EmailAddress]
+        public string Email { get; set; }
+
+        //int is automatically required because type=int
+        [Key]//This is Primary key
+        public int MemberId { get; set; }
+
+        [DataType(DataType.Password)]
+        [Required]
+        public string Password { get; set; }
+
+    }
+}
